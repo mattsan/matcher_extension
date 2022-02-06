0a09104aa6da49268decaed37563dd4b4046d1fc
Eiji MATSUMOTO <e.mattsan@gmail.com>
2022-02-06 10:15:34 +0900

    add dialyxir's configurations for GitHub Actions (2)

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule MatcherExtension.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: dialyzer()
     ]
   end
 
@@ -22,6 +23,12 @@ defmodule MatcherExtension.MixProject do
       {:credo, "~> 1.6", only: :dev, runtime: false},
       {:dialyxir, "~> 1.1", only: :dev, runtime: false},
       {:ex_doc, "~> 0.28.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp dialyzer do
+    [
+      plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
     ]
   end
 end
