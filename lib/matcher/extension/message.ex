0a2e63fc5fb6eaaf668ae217d8c5a7baa52e49f0
Eiji MATSUMOTO <e.mattsan@gmail.com>
2022-02-06 13:43:17 +0900

    clean up Message module

--- lib/matcher/extension/message.ex
+++ lib/matcher/extension/message.ex
@@ -1,47 +1,35 @@
 defmodule Matcher.Extension.Message do
   @moduledoc false
 
-  def format(expression, %{from: expected}, %{from: actual}) when expected != actual do
-    IO.chardata_to_string([
-      "expected ",
-      inspect(expression),
-      " to have initially been ",
-      inspect(expected),
-      ", but was ",
-      inspect(actual)
-    ])
-  end
-
-  def format(expression, %{to: expected}, %{to: actual}) do
-    IO.chardata_to_string([
-      "expected ",
-      inspect(expression),
-      " to have changed to ",
-      inspect(expected),
-      ", but is now ",
-      inspect(actual)
-    ])
-  end
-
-  def format(expression, %{by: expected}, %{by: actual}) do
-    IO.chardata_to_string([
-      "expected ",
-      inspect(expression),
-      " to have changed by ",
-      inspect(expected),
-      ", but was changed by ",
-      inspect(actual)
-    ])
-  end
-
   def format(expression, expected, actual) do
-    IO.chardata_to_string([
-      "expected ",
-      inspect(expression),
-      " not to have changed, but did change from ",
-      inspect(expected),
-      " to ",
-      inspect(actual)
-    ])
+    iodata =
+      case {expected, actual} do
+        {%{from: expected}, %{from: actual}} when expected != actual ->
+          [
+            [" to have initially been ", expected],
+            [", but was ", actual]
+          ]
+
+        {%{to: expected}, %{to: actual}} ->
+          [
+            [" to have changed to ", expected],
+            [", but is now ", actual]
+          ]
+
+        {%{by: expected}, %{by: actual}} ->
+          [
+            [" to have changed by ", expected],
+            [", but was changed by ", actual]
+          ]
+
+        {expected, actual} ->
+          [
+            [" not to have changed, but did change from ", expected],
+            [" to ", actual]
+          ]
+      end
+      |> Enum.map(fn [first, second] -> [first, inspect(second)] end)
+
+    IO.iodata_to_binary([["expected ", inspect(expression)], iodata])
   end
 end
